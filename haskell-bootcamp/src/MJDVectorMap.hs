2e7f6d8cf69afff8b8eb91495afbea891dcacdae
Mark Dominus <mjd@leapyear.io>
2022-07-15 09:42:03 -0400

    Map class based on mutable vectors

--- haskell-bootcamp/src/MJDVectorMap.hs
+++ haskell-bootcamp/src/MJDVectorMap.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module MJDVectorMap where
 
-import Control.Monad (join)
 import Data.Char (ord)
 import Data.Maybe
-import Data.Monoid as M
 import Data.Vector (Vector)
 import qualified Data.Vector as V
+import qualified Data.Vector.Generic.Mutable as VM
 import GHC.Base (undefined)
+import GHC.Real
+import GHC.Stack
 import Prelude hiding (splitAt)
 
 -- Eventually (when we do `delete`) we'll need a three-headed type
 -- that includes tombstones, but not yet
-newtype Map k v
-    = Map (Vector (Maybe (k, v)))
+data Map k v = Map
+    { slots :: (Vector (Slot k v))
+    , fullness :: Integer
+    }
     deriving (Show)
 
-unMap (Map x) = x
+-- applies f over the values
+instance Functor (Map k) where
+    fmap f m@(Map{slots = vec}) = m{slots = (fmap . fmap . fmap) f vec}
+
+-- might be simpler with 'do' notation
+{- mapKeys :: (k -> k') -> Map k v -> Map k' v
+mapKeys f (Map{slots = vec}) = Map $ fmap (maymap (\(k, v) -> (f k, v))) vec
+  where
+    maymap :: (a -> b) -> Maybe a -> Maybe b
+    maymap = fmap -}
 
 class MJDHashable t where
     hash :: t -> Int
@@ -25,10 +38,8 @@ class MJDHashable t where
 instance MJDHashable Char where
     hash = ord
 
--- M.Sum isn't the right thing to use here, I just did it because it was as simple as possible
 instance (Foldable f, MJDHashable a) => MJDHashable (f a) where
-    -- hash as = getSum $ foldMap (M.Sum . hash) as
-    hash as = 0
+    hash = foldr (\a b -> 7 * b + (hash a)) 0
 
 -- Make a new vector that is like the old one, but starting at
 -- a different place and wrapping around
@@ -37,17 +48,12 @@ wrapV start vec = back V.++ front
   where
     (front, back) = V.splitAt start (V.indexed vec)
 
--- Fold a Vector, but start in the middle somewhere
--- and wrap around from the end to the beginning
--- (not currently used)
-{- wrapFoldr :: (Maybe (k, v) -> b -> b) -> b -> Int -> Map k v -> b
-wrapFoldr f init startPos (Map v) =
-    V.foldr f init (wrapV startPos v) -}
-
 type Slot k v = Maybe (k, v)
 
+-- look up a key in the map, return the number of the slot
+-- where it was found, or where it should be inserted if it's not there yet
 getIndex :: (Eq k, MJDHashable k) => k -> Map k v -> Maybe Int
-getIndex k m = fmap fst $ V.find rightSlot (wrapV start $ unMap m)
+getIndex k m = fmap fst $ V.find rightSlot (wrapV start $ slots m)
   where
     start = hash k `mod` size m
     -- Stop searching if we find an empty slot...
@@ -56,46 +62,82 @@ getIndex k m = fmap fst $ V.find rightSlot (wrapV start $ unMap m)
     rightSlot (_, Just (k', _)) = k' == k
 
 get :: (Eq k, MJDHashable k) => k -> Map k v -> Maybe v
-get k m = fmap snd $ join $ ((unMap m) V.!) <$> i
+get k m@(Map{slots = vec}) = do
+    i <- getIndex k m
+    (_, v) <- vec V.! i
+    pure v
+
+insert :: (HasCallStack, Eq k, MJDHashable k) => k -> v -> Map k v -> Map k v
+insert k v m@(Map{slots}) =
+    if density m > 0.5
+        then insert k v (doubleSize m)
+        else
+            Map
+                { slots = update1' slots i (Just (k, v))
+                , -- BRANDON
+                  fullness = 1 + fullness m -- not necessarily
+                }
   where
-    i = getIndex k m
+    update1 vec i v = vec V.// [(i, v)]
+    update1' vec i v = V.modify (\vec' -> VM.write vec' i v) vec
+    i = fromMaybe err (getIndex k m)
+    err = error "can't insert into full table"
 
-sample :: Map String String
-sample = Map $ V.fromList [Just ("apple", "red"), Just ("banana", "yellow"), Nothing, Just ("kiwi", "brown")]
+insertPair :: (Eq k, MJDHashable k) => (k, v) -> Map k v -> Map k v
+insertPair (k, v) m = insert k v m
 
-{- findIndex k m = (wrappedIndex + (size m))
-    where
-    wrappedIndex = (join . V.find rightPair) (wrapV start $ unMap m)
-    start = hash k `mod` size m
-    -- Stop searching if we find an empty slot...
-    rightPair Nothing = True
-    -- ... or if we find a full slot with a matching key
-    rightPair (Just (k', _)) = k' == k -}
-
--- for later: destructive update here?
--- ways to do this:
---   1. implement a version of `get` that, instead of returning the value,
---      returns the index at which it was found.  This is tricky because of the way
---      you implemented `wrapV`; it doesn't know the indices any more
---   2. map over an initial segment of the whole vector, updating only the one
---      matching element, and use `V.concat` to reuse the tail of the vector
-
-{- insert :: k -> v -> Map k v -> Map k v
-insert k v m =
-    undefined
-  where
-    start = hash k `mod` size m
-    -- update if the slot is empty
-    newPair Nothing = Just (k, v)
-    -- ... or overwrite if we find a full slot with a matching key
-    newPair (Just (k', _)) | k' == k = Just (k, v) -}
-
---
-
-lift f = Map . f . unMap
-
--- instance Foldable (Map k) where  -- Why doesn't this work?
---    foldMap f (Map v) = foldMap f v
+doubleSize :: (Eq k, MJDHashable k) => Map k v -> Map k v
+doubleSize m = fromListWithSize (2 * size m) (getItems m)
 
 size :: Map k v -> Int
-size = V.length . unMap
+size = V.length . slots
+
+a :: Int
+a = 3
+
+density :: Map k v -> Double
+density m = (fromIntegral $ fullness m) / (fromIntegral $ size m)
+
+emptyMap :: Int -> Map k v
+emptyMap n
+    | n < 4 =
+        error "Minimum size for a map is 4 slots"
+    | otherwise =
+        Map
+            { slots = V.replicate n Nothing
+            , fullness = 0
+            }
+
+fromListWithSize :: (MJDHashable k, Eq k) => Int -> [(k, v)] -> Map k v
+fromListWithSize newSize items =
+    foldl
+        (\m -> \(k, v) -> insert k v m)
+        (emptyMap newSize)
+        items
+
+fromList :: (MJDHashable k, Eq k) => [(k, v)] -> Map k v
+fromList ls = fromListWithSize (5 * length ls) ls
+
+getKeys :: Map k v -> [k]
+getKeys = map fst . getItems
+
+getItems :: Map k v -> [(k, v)]
+getItems (Map{slots = vec}) = [(k, v) | Just (k, v) <- V.toList vec]
+
+sampleKVPs :: [(String, String)]
+sampleKVPs = [("potato", "blue"), ("apple", "red"), ("banana", "yellow"), ("kiwi", "brown"), ("blackberry", "black"), ("octopus", "purple")]
+
+sample :: Map String String
+sample = fromListWithSize 8 sampleKVPs
+
+lift :: (Vector (Slot k v) -> Vector (Slot k v)) -> (Map k v -> Map k v)
+lift f m@Map{slots} = m{slots = f slots}
+
+-- instance Foldable (Map k) wher:re  -- Why doesn't this work?
+--    foldMap f (Map v) = foldMap f v
+
+-- For debugging, thanks Deech
+-- important feature: if the pattern match fails, the
+-- element is just skipped silently
+display :: Map k v -> [(Int, k, v)]
+display m = [(i, k, v) | (i, Just (k, v)) <- (V.toList . V.indexed . slots) m]
