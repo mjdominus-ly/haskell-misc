27d975100df0ad96c48b3486ebeb99dfb85ec17e
Mark Dominus <mjd@leapyear.io>
2022-08-30 14:55:41 -0400

    more variations on directory scanner

--- monad-stacking/src/DirList.hs
+++ monad-stacking/src/DirList.hs
@@ -5,44 +5,104 @@ import MJDState
 import System.Directory
 import System.IO
 
-newtype FileInfo = FI (FilePath, Integer)
+newtype FileInfo = FI (FilePath, Integer) deriving (Eq, Show)
+data FIS = FIS {todo :: [FilePath], result :: [FileInfo]} deriving (Show)
 
-{- data FileIterationState b = FIS {todo :: [FilePath], result :: [b]}
+inDir d fp = d ++ "/" ++ fp
+qualify d = map (inDir d)
 
-nextUp tSTM = do
-    t <- get
-    case t of
-        FIS{todo = [], result = r} -> return $ (t, ())
-        FIS{todo = (f : fs), result = r} -> do
-            info <- fileInfo f
-            (FIS{todo = fs, result = fileInfo f : r}, ()) -}
+getTodo :: StateT FIS m [FilePath]
+getTodo = fmap todo get
 
-accumulate :: (Show s, Monad m) => s -> StateT [s] m ()
-accumulate x = (x :)
+ifEmpty :: Monad m => m [x] -> m a -> m a -> m a
+ifEmpty mls mt me = do
+    ls <- mls
+    case ls of
+        [] -> mt
+        _ -> me
 
-pass :: Monad m => m ()
-pass = return ()
+step' :: StateT FIS IO ()
+step' = do
+    ifEmpty
+        getTodo
+        (return ())
+        ( do
+            f : fs <- getTodo
+            exists <- liftIO $ doesFileExist f
+            res <-
+                if exists
+                    then do
+                        info <- liftIO $ fileInfo f
+                        pure (info : result)
+                    else pure result
+            put $ FIS{todo = fs, result = res}
+            step'
+        )
 
-addFileInfo :: FilePath -> StateT [(FilePath, Integer)] IO ()
+step :: StateT FIS IO ()
+step = do
+    FIS todo result <- get
+    case todo of
+        [] -> return ()
+        (f : fs) -> do
+            exists <- liftIO $ doesFileExist f
+            res <-
+                if exists
+                    then do
+                        info <- liftIO $ fileInfo f
+                        pure (info : result)
+                    else pure result
+            put $ FIS{todo = fs, result = res}
+            step
+
+initState :: [FilePath] -> FIS
+initState files = FIS{todo = files, result = []}
+
+main3 dir = do
+    files <- listDirectory dir
+    let paths = qualify dir files
+    (final_state, _) <- run step (initState paths)
+    return final_state
+
+step2 :: StateT [FilePath] IO [FileInfo]
+step2 = do
+    todo <- get
+    case todo of
+        [] -> return []
+        (f : fs) -> do
+            put fs
+            is <- step2
+            exists <- liftIO $ doesFileExist f
+            if exists
+                then do
+                    i <- liftIO $ fileInfo f
+                    return (i : is)
+                else return is
+
+main4 dir = do
+    files <- listDirectory dir
+    let paths = map (inDir dir) files
+    (_, v) <- run step2 paths
+    return v
+
+addFileInfo :: FilePath -> StateT [FileInfo] IO ()
 addFileInfo f = do
     exists <- liftIO $ doesFileExist f
     if exists
         then do
             v <- liftIO $ fileInfo f
-            accumulate v
-        else pass
+            update (v :)
+        else return ()
 
-fileInfo :: FilePath -> IO (FilePath, Integer)
+fileInfo :: FilePath -> IO FileInfo
 fileInfo fp = do
     size <- fileSize fp
-    return (fp, size)
+    return $ FI (fp, size)
 
 fileSize :: FilePath -> IO (Integer)
 fileSize fp = withFile fp ReadMode hFileSize
 
-inDir d fp = d ++ "/" ++ fp
-
-mainn :: FilePath -> IO [(FilePath, Integer)]
+mainn :: FilePath -> IO [FileInfo]
 mainn dir = do
     files <- listDirectory dir
     let paths = map (inDir dir) files
